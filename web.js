65b840494a085197a9cb274a79d5b524be980370
Seyed Zayeni <seyed.zayeni@gmail.com>
2013-07-07 16:46:26 +0000

    avec les nouveaux ajouts

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+var ficher = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fichier.toString('utf8',0,len));
 });
 
 var port = process.env.PORT || 5000;
